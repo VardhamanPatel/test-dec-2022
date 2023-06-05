3415fe6f70a247f4865b2d58af1e85930bc75ad3
Vardhaman Patel <vardhmanpatel733@gmail.com>
2023-06-05 10:26:50 +0530

    Simply Created a Spring Boot Application

---
+++ First_Spring_Boot_App/src/main/java/com/example/First_Spring_Boot_App/FirstSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.First_Spring_Boot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ First_Spring_Boot_App/src/test/java/com/example/First_Spring_Boot_App/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First_Spring_Boot_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
